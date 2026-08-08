9815798db2f905bc24bd33d08df993bef8d8fcad
Jinghao Hu <731409213@qq.com>
2026-08-08 18:37:45 +0800

    Expose the compact public API

--- src/pysurveying/__init__.py
+++ src/pysurveying/__init__.py
@@ -1,5 +1,6 @@
 """pySurveying: lightweight surveying computation and adjustment."""
 
+from .adjustment import adjust_control_network, adjust_free_network, least_squares
 from .basic import (
     azimuth,
     degree_to_dms,
@@ -12,7 +13,21 @@ from .basic import (
     normalize_angle,
     resection,
 )
+from .engineering import offset_point, polar_stakeout, polygon_area, slope
+from .leveling import leveling_route
 from .models import AdjustmentResult, Observation, Point
+from .quality import (
+    detect_outliers,
+    error_ellipse,
+    robust_least_squares,
+    standardized_residuals,
+)
+from .transform import transform_coordinates
+from .traverse import (
+    closed_traverse,
+    connected_traverse,
+    traverse_azimuths_from_angles,
+)
 
 __all__ = [
     "Point",
@@ -28,6 +43,22 @@ __all__ = [
     "normalize_angle",
     "dms_to_degree",
     "degree_to_dms",
+    "closed_traverse",
+    "connected_traverse",
+    "traverse_azimuths_from_angles",
+    "leveling_route",
+    "least_squares",
+    "adjust_control_network",
+    "adjust_free_network",
+    "robust_least_squares",
+    "standardized_residuals",
+    "detect_outliers",
+    "error_ellipse",
+    "transform_coordinates",
+    "polar_stakeout",
+    "offset_point",
+    "slope",
+    "polygon_area",
 ]
 
 __version__ = "0.1.0"
